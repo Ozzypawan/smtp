4cb3fde90a267a51b4186807ac79c873df98b761
Ozzypawan <pawanpandey1251@gmail.com>
2024-09-26 17:51:54 +0545

    first commit

--- smtp/settings.py
+++ smtp/settings.py
@@ -128,8 +128,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'ayatadev@gmail.com' 
-EMAIL_HOST_PASSWORD = 'tjxo fqis gxcx ngrw' 
+EMAIL_HOST_USER = 'pawanpandey1251@gmail.com' 
+EMAIL_HOST_PASSWORD = 'tjxo fqis gxcx ngrz' 
 
 # smtp/settings.py
 
